1313f01a7708bf5af62ab3eccedaa5d9d678706c
patrycjakaliciak <patrycja.kaliciak@student.wat.edu.pl>
2024-11-25 13:42:08 +0100

    chore: add new users

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ users:list=[
     {'name':'Szymon z wąsem','posts':5,'city':'Toruń'},
     {'name':'Szymon','posts':7,'city':'Łódź'},
     {'name':'Patryk','posts':9,'city':'Kielce'},
+    {'name':'Żerom','posts':10,'city':'Katowice'},
+    {'name':'Michał','posts':11,'city':'Zielona Góra'},
+    {'name':'Dominik','posts':12,'city':'Szczecin'},
+    {'name':'Kinga','posts':13,'city':'Bydgoszcz'},
+    {'name':'Aleksandra','posts':14,'city':'Gdańsk'},
+
 
 ]
 #TODO please update user list
